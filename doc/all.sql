afcc45f2623d959a527e3d251e68d16fab820a68
yao <1648031517@qq.com>
2021-07-24 10:44:06 +0800

    3-3 增加了all.sql脚本

---
+++ doc/all.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `name` VARCHAR(50) COMMENT '名称',
+    `password` VARCHAR(50) COMMENT '密码',
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='测试';
+
+INSERT INTO `test` (id, NAME, PASSWORD) VALUES (1, '测试', 'password');

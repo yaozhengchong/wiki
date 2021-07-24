543789b12c4530ac1c9b2db2563210cf84e6dab9
yao <1648031517@qq.com>
2021-07-24 16:08:56 +0800

    3-5 集成mybatis generator,生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,7 +6,14 @@ CREATE TABLE `test` (
     `password` VARCHAR(50) COMMENT '密码',
     PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='测试';
-
 INSERT INTO `test` (id, NAME, PASSWORD) VALUES (1, '测试', 'password');
-
 select * from test;
+
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo` (
+    `id` BIGINT NOT NULL COMMENT 'id',
+    `name` VARCHAR(50) COMMENT '名称',
+    PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 COMMENT='测试';
+INSERT INTO `demo` (id, NAME) VALUES (1, '测试');
+select * from demo;

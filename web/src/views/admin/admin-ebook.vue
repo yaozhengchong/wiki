c7cef9442e0d0beb9dc8834466ae0e53e4795ee9
yao <1648031517@qq.com>
2021-07-26 20:43:32 +0800

    6-12 增加按名字查询电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -4,9 +4,24 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
-        <a-button type="primary" @click="add()" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+              <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add()">
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
+
       </p>
       <a-table
               :columns="columns"
@@ -74,6 +89,8 @@
   export default defineComponent({
     name: 'AdminEbook',
     setup() {
+      const param = ref();
+      param.value = {};
       const ebooks = ref();
       const pagination = ref({
         current: 1,
@@ -128,7 +145,8 @@
         axios.get("/ebook/list", {
           params: {
             page: params.page,
-            size: params.size
+            size: params.size,
+            name: param.value.name
           }
         }).then((response) => {
           loading.value = false;
@@ -223,11 +241,13 @@
       });
 
       return {
+        param,
         ebooks,
         pagination,
         columns,
         loading,
         handleTableChange,
+        handleQuery,
 
         edit,
         add,

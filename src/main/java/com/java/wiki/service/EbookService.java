ad1ad5a5a9f9d3b433ab907340561eb092a6902a
yao <1648031517@qq.com>
2021-07-26 16:30:27 +0800

    6-8 完成电子书编辑功能，编辑成功后刷新列表 查询接口参数改为EbookQueryResp,EbookQueryResp

--- src/main/java/com/java/wiki/service/EbookService.java
+++ src/main/java/com/java/wiki/service/EbookService.java
@@ -5,8 +5,9 @@ import com.github.pagehelper.PageInfo;
 import com.java.wiki.domain.Ebook;
 import com.java.wiki.domain.EbookExample;
 import com.java.wiki.mapper.EbookMapper;
-import com.java.wiki.req.EbookReq;
-import com.java.wiki.resp.EbookResp;
+import com.java.wiki.req.EbookQueryReq;
+import com.java.wiki.req.EbookSaveReq;
+import com.java.wiki.resp.EbookQueryResp;
 import com.java.wiki.resp.PageResp;
 import com.java.wiki.util.CopyUtil;
 import org.slf4j.Logger;
@@ -25,7 +26,7 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public PageResp<EbookResp> list(EbookReq req){
+    public PageResp<EbookQueryResp> list(EbookQueryReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
@@ -48,13 +49,26 @@ public class EbookService {
         //}
 
         //列表复制
-        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-        PageResp<EbookResp> pageResp = new PageResp<>();
+        List<EbookQueryResp> list = CopyUtil.copyList(ebookList, EbookQueryResp.class);
+        PageResp<EbookQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list);
         return pageResp;
     }
 
-
-
+    /**
+     * 保存
+     * @param req
+     */
+    public void save(EbookSaveReq req) {
+        Ebook ebook = CopyUtil.copy(req, Ebook.class);
+        if (ObjectUtils.isEmpty(req.getId())){
+            //新增
+            ebookMapper.insert(ebook);
+        }else{
+            //更新
+            ebookMapper.updateByPrimaryKey(ebook);
+        }
+        ebookMapper.updateByPrimaryKey(ebook);
+    }
 }

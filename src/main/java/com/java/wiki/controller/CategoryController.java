bfb36e1e5e18457990b1ddb74341ed139112eb5e
yao <1648031517@qq.com>
2021-07-27 16:53:38 +0800

    7-4 分类管理不显示分页

--- src/main/java/com/java/wiki/controller/CategoryController.java
+++ src/main/java/com/java/wiki/controller/CategoryController.java
@@ -1,29 +1,42 @@
 package com.java.wiki.controller;
 
+import com.java.wiki.req.CategoryQueryReq;
+import com.java.wiki.req.CategorySaveReq;
+import com.java.wiki.resp.CategoryQueryResp;
 import com.java.wiki.resp.CommonResp;
 import com.java.wiki.resp.PageResp;
+import com.java.wiki.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
     @Resource
-    private com.java.wiki.service.CategoryService categoryService;
+    private CategoryService categoryService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<CategoryQueryResp>> resp = new CommonResp<>();
+        List<CategoryQueryResp> list = categoryService.all();
+        resp.setContent(list);
+        return resp;
+    }
 
     @GetMapping("/list")
-    public CommonResp list(@Valid com.java.wiki.req.CategoryQueryReq req) {
-        CommonResp<PageResp<com.java.wiki.resp.CategoryQueryResp>> resp = new CommonResp<>();
-        PageResp<com.java.wiki.resp.CategoryQueryResp> list = categoryService.list(req);
+    public CommonResp list(@Valid CategoryQueryReq req) {
+        CommonResp<PageResp<CategoryQueryResp>> resp = new CommonResp<>();
+        PageResp<CategoryQueryResp> list = categoryService.list(req);
         resp.setContent(list);
         return resp;
     }
 
     @PostMapping("/save")
-    public CommonResp save(@Valid @RequestBody com.java.wiki.req.CategorySaveReq req) {
+    public CommonResp save(@Valid @RequestBody CategorySaveReq req) {
         CommonResp resp = new CommonResp<>();
         categoryService.save(req);
         return resp;

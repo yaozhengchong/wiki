7accb7c42fc0907dad616b53796ea6686f156342
yao <1648031517@qq.com>
2021-07-28 17:02:19 +0800

    8-6 删除文档，增加二次确认

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -78,11 +78,13 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref } from 'vue';
   import axios from 'axios';
-  import { message } from 'ant-design-vue';
   import {Tool} from "@/util/tool";
   import {useRoute} from "vue-router";
+  import {createVNode, defineComponent, onMounted, ref} from "vue";
+  import {Modal} from "ant-design-vue";
+  import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
+  import deleteNames from "@vue/compiler-sfc/dist/compiler-sfc.esm-browser";
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -211,7 +213,8 @@
         }
       };
 
-      const ids: Array<String> = [];
+      const deleteIds: Array<String> = [];
+      const deleteNames: Array<String> = [];
       /**
        * 查找整根树枝
        */
@@ -225,7 +228,8 @@
             console.log("delete", node);
             // 将目标节点设置为disabled
             node.disabled = true;
-            ids.push(id);
+            deleteIds.push(id);
+            deleteNames.push(node.name);
 
             // 遍历所有子节点
             const children = node.children;
@@ -272,12 +276,19 @@
 
       const handleDelete = (id: number) => {
         getDeleteIds(level1.value, id);
-        axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
-          const data = response.data; // data = commonResp
-          if (data.success) {
-            // 重新加载列表
-            handleQuery();
-          }
+        Modal.confirm({
+          title: '再次提醒',
+          icon: createVNode(ExclamationCircleOutlined),
+          content: '将删除：【' + deleteNames.join(". ") + "】删除后不可恢复，确认删除？",
+          onOk() {
+            axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+              const data = response.data; // data = commonResp
+              if (data.success) {
+                // 重新加载列表
+                handleQuery();
+              }
+            });
+          },
         });
       };
 

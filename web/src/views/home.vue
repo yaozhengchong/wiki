f62f9e712e2fd3085555db4a8c21d77146dcfab6
yao <1648031517@qq.com>
2021-08-02 17:53:43 +0800

    8-12 点击文档节点时，记载并显示文档内容

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,4 +1,4 @@
-<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+<template>
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
@@ -7,7 +7,7 @@
               @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <MailOutlined/>
+          <MailOutlined />
           <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
@@ -15,16 +15,16 @@
             <span><user-outlined />{{item.name}}</span>
           </template>
           <a-menu-item v-for="child in item.children" :key="child.id">
-            <MailOutlined/><span>{{child.name}}</span>
+            <MailOutlined /><span>{{child.name}}</span>
           </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-  >
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎使用知识库</h1>
+        <h1>欢迎使用甲蛙知识库</h1>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -41,133 +41,122 @@
                   {{ item.name }}
                 </router-link>
               </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
+              <template #avatar><a-avatar :src="item.cover"/></template>
             </a-list-item-meta>
           </a-list-item>
         </template>
       </a-list>
-  </a-layout-content>
+    </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
-import axios from 'axios';
-import { message } from 'ant-design-vue';
-import {Tool} from "@/util/tool";
+  import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+  import axios from 'axios';
+  import { message } from 'ant-design-vue';
+  import {Tool} from "@/util/tool";
 
+  // const listData: any = [];
+  // for (let i = 0; i < 23; i++) {
+  //   listData.push({
+  //     href: 'https://www.antdv.com/',
+  //     title: `ant design vue part ${i}`,
+  //     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+  //     description:
+  //         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+  //     content:
+  //         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  //   });
+  // }
 
-// const listData: Record<string, string>[] = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'https://www.antdv.com/',
-//     title: `ant design vue part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
+  export default defineComponent({
+    name: 'Home',
+    setup() {
+      const ebooks = ref();
+      // const ebooks1 = reactive({books: []});
 
-export default defineComponent({
-  name: 'Home',
-  setup(){
-    const ebooks = ref();
-    // const ebooks1 = reactive({books: []});
+      const level1 =  ref();
+      let categorys: any;
+      /**
+       * 查询所有分类
+       **/
+      const handleQueryCategory = () => {
+        axios.get("/category/all").then((response) => {
+          const data = response.data;
+          if (data.success) {
+            categorys = data.content;
+            console.log("原始数组：", categorys);
 
-    const level1 =  ref();
-    let categorys: any;
-    /**
-     * 查询所有分类
-     **/
-    const handleQueryCategory = () => {
-      axios.get("/category/all").then((response) => {
-        const data = response.data;
-        if (data.success) {
-          categorys = data.content;
-          console.log("原始数组：", categorys);
+            level1.value = [];
+            level1.value = Tool.array2Tree(categorys, 0);
+            console.log("树形结构：", level1.value);
+          } else {
+            message.error(data.message);
+          }
+        });
+      };
 
-          level1.value = [];
-          level1.value = Tool.array2Tree(categorys, 0);
-          console.log("树形结构：", level1.value);
+      const isShowWelcome = ref(true);
+      let categoryId2 = 0;
+
+      const handleQueryEbook = () => {
+        axios.get("/ebook/list", {
+          params: {
+            page: 1,
+            size: 1000,
+            categoryId2: categoryId2
+          }
+        }).then((response) => {
+          const data = response.data;
+          ebooks.value = data.content.list;
+          // ebooks1.books = data.content;
+        });
+      };
+
+      const handleClick = (value: any) => {
+        // console.log("menu click", value)
+        if (value.key === 'welcome') {
+          isShowWelcome.value = true;
         } else {
-          message.error(data.message);
+          categoryId2 = value.key;
+          isShowWelcome.value = false;
+          handleQueryEbook();
         }
+        // isShowWelcome.value = value.key === 'welcome';
+      };
+
+      onMounted(() => {
+        handleQueryCategory();
+        // handleQueryEbook();
       });
-    };
 
-    const isShowWelcome = ref(true);
-    let categoryId2 = 0;
-
-    /**
-     * 查询电子书
-     */
-    const handleQueryEbook = () => {
-      axios.get("/ebook/list", {
-        params: {
-          page: 1,
-          size: 1000,
-          categoryId2: categoryId2
-        }
-      }).then((response) => {
-        const data = response.data;
-        ebooks.value = data.content.list;
-        // ebooks1.books = data.content;
-      });
-    };
-
-    /**
-     * 点击调用
-     */
-    const handleClick = (value: any) => {
-      // console.log("menu click", value);
-      if(value.key === 'welcome'){
-        isShowWelcome.value = true;
-      }else{
-        categoryId2 = value.key;
-        isShowWelcome.value = false;
-        handleQueryEbook();
-      }
-      // isShowWelcome.value = value.key === 'welcome';
-    };
-
-    /**
-     * 初始调用
-     */
-    onMounted(() =>{
-      handleQueryCategory();
-      // handleQueryEbook();
-    });
-
-    return{
-      ebooks,
-      // ebooks2: toRef(ebooks1, "books"),
-      // listData,
-      pagination:{
-        onChange: (page: number) => {
-          console.log(page);
+      return {
+        ebooks,
+        // ebooks2: toRef(ebooks1, "books"),
+        // listData,
+        pagination: {
+          onChange: (page: any) => {
+            console.log(page);
+          },
+          pageSize: 3,
         },
-        pageSize: 3,
-      },
-      actions:[
-        { type: 'StarOutlined', text: '156' },
-        { type: 'LikeOutlined', text: '156' },
-        { type: 'MessageOutlined', text: '2' },
-      ],
+        actions: [
+          { type: 'StarOutlined', text: '156' },
+          { type: 'LikeOutlined', text: '156' },
+          { type: 'MessageOutlined', text: '2' },
+        ],
 
-      handleClick,
-      level1,
+        handleClick,
+        level1,
 
-      isShowWelcome,
-
+        isShowWelcome
+      }
     }
-  }
-});
+  });
 </script>
 
 <style scoped>
-  .ant-avatar{
+  .ant-avatar {
     width: 50px;
     height: 50px;
     line-height: 50px;

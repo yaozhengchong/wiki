4c93b8aae7bb3449baaf55656217f226dd2f4a61
yao <1648031517@qq.com>
2021-08-02 23:42:17 +0800

    9-3 完成用户表基本增删改查功能

---
+++ src/main/java/com/java/wiki/req/UserQueryReq.java
@@ -0,0 +1,23 @@
+package com.java.wiki.req;
+
+import com.java.wiki.req.PageReq;
+
+public class UserQueryReq extends PageReq {
+
+    private String loginName;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserQueryReq{" +
+                "loginName='" + loginName + '\'' +
+                "} " + super.toString();
+    }
+}

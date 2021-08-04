e90272bce4148fd54cbbbe63bef8c0a0616a953e
yao <1648031517@qq.com>
2021-08-04 18:12:01 +0800

    9-10 前后端增加登录拦截，修复BUG:前后端存储token,统一用String类型

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,6 +6,7 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
+import {Tool} from "@/util/tool";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
@@ -14,6 +15,11 @@ axios.defaults.baseURL = process.env.VUE_APP_SERVER;
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+    const token = store.state.user.token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
@@ -27,11 +33,11 @@ axios.interceptors.response.use(function (response) {
 });
 
 const app = createApp(App);
-app.use(store).use(router).use(Antd).mount('#app')
+app.use(store).use(router).use(Antd).mount('#app');
 
-//全局使用图标
+// 全局使用图标
 const icons: any = Icons;
-for (const i in icons){
+for (const i in icons) {
     app.component(i, icons[i]);
 }
 

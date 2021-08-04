b912b91ab84ccc911035d3f1dfcba9fabc317f3d
yao <1648031517@qq.com>
2021-08-04 10:09:46 +0800

    9-9 增加redis测试接口

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -2,44 +2,68 @@ package com.java.wiki.controller;
 
 import com.java.wiki.domain.Test;
 import com.java.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
 
-    @Value("${test.hello:Test}")
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello:TEST}")
     private String testHello;
 
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
     /**
-     * GET,POST,PUT,DELETE
+     * GET, POST, PUT, DELETE
      *
      * /user?id=1
      * /user/1
      * @return
      */
-    //http://127.0.0.1:8880/hello
-    //@GetMapping
-    //@PostMapping
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World!" + testHello;
     }
 
-    @PostMapping(value = "/hello/post")
-    public String hello(String name){
-        return "hello World! Post，" + name;
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 
     @GetMapping("/test/list")
-    public List<Test> list(){
+    public List<Test> list() {
         return testService.list();
     }
 
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
 
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
+    }
 }

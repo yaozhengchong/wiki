34778f6595753ceacab1f6b2ea9f362d5ce88688
yao <1648031517@qq.com>
2021-08-05 16:09:13 +0800

    10-10 将业务线程日志流水号传递到异步线程，方便生产运维查问题

--- src/main/java/com/java/wiki/service/DocService.java
+++ src/main/java/com/java/wiki/service/DocService.java
@@ -21,6 +21,7 @@ import com.java.wiki.util.SnowFlake;
 import com.java.wiki.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -164,7 +165,8 @@ public class DocService {
 
         //推送消息
         Doc docDB = docMapper.selectByPrimaryKey(id);
-        wsService.sendInfo("【" + docDB.getName() + "】被点赞!");
+        String logId = MDC.get("LOG_ID");
+        wsService.sendInfo("【" + docDB.getName() + "】被点赞!",logId);
     }
 
     public void updateEbookInfo(){
--- src/main/java/com/java/wiki/service/WsService.java
+++ src/main/java/com/java/wiki/service/WsService.java
@@ -2,6 +2,7 @@ package com.java.wiki.service;
 
 import com.java.wiki.domain.Doc;
 import com.java.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,8 @@ public class WsService {
     public WebSocketServer webSocketServer;
 
     @Async
-    public void sendInfo(String message){
+    public void sendInfo(String message,String logId){
+        MDC.put("LOG_ID",logId);
         webSocketServer.sendInfo(message);
     }
 }
